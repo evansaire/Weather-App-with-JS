1220c14e7128b16b3b8146e6311374c5f9de1d9b
Evans Edosa <edosaosagieevans@gmail.com>
2020-07-12 18:53:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ function displayWeather(weather) {
 
 function dateBuild(d) {
     let months =["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
-    let days =["Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday","Sunday"]
+    let days =["Sunday","Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday"]
 
     let day = days[d.getDay()];
     let date = d.getDate();
